b447652e6a5a4af2ac5fcce6428cd52dfb0b0f07
horleng <ly.horleng.1221@rupp.edu.kh>
2023-03-11 09:56:28 +0700

    modify

--- src/Join.jsx
+++ src/Join.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 const Join = () => {
     const [room,setRoom] = useState("");
     useEffect(()=>{
-      console.log(1)
       const inp = document.querySelector('#room');
       inp.addEventListener('focusin',()=>{
         document.querySelector(".label").classList.remove('top-8');
@@ -28,7 +27,7 @@ const Join = () => {
           <div className='relative top-[100px] flex flex-col md:w-[400px] w-[300px]'>
             <label htmlFor="room" className='label absolute text-base md:left-[40%] left-[35%] text-cyan-200 transition-all duration-200 top-8'>Group's name</label>
             <input value={room} id='room' onChange={(e)=>setRoom(e.target.value)} type="text"
-            className='pt-8 pb-0 md:px-[40%] px-[35%] placeholder:text-sm outline-none text-sky-700 text-base bg-inherit border-b-[3px] border-sky-400'/>
+            className='pt-8 pb-0 md:pl-[40%] pl-[35%] placeholder:text-sm outline-none text-sky-700 text-base bg-inherit border-b-[3px] border-sky-400'/>
           <Link to={"/"+room} className='mx-auto'>
             <button className='md:px-16 px-10 py-2 mt-5 rounded-md bg-sky-500 text-sm hover:bg-sky-400 transition-all shadow-md shadow-sky-700'>Join</button>
           </Link> 
--- src/videoCall.jsx
+++ src/videoCall.jsx
@@ -1,43 +1,162 @@
-import React, {useRef } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import {io} from "socket.io-client";
 import { Peer } from "peerjs";
+import {SlScreenDesktop} from "react-icons/sl";
+import {MdOutlineFlipCameraIos} from "react-icons/md";
+import {AiFillVideoCamera} from "react-icons/ai";
 const Chat = () => {
     const {room} = useParams();
-    const myVideoRef = useRef();
-    const friVideoRef = useRef();
-    const peers = {};
     const peer = new Peer();
+    const peers = {};
     const socket = io("https://video-call-ob4g.onrender.com");
-    peer.on("open",id=>{
+    var videoUnique = [];
+    var i=0;
+    peer.on("open",(id)=>{
+        openCamera();
+        window.id = id;
         socket.emit("join",{room,id});
-    })
-    navigator.mediaDevices.getUserMedia({video:{facingMode: {exact:"user"},},audio:true})
-    .then(ownVideo=>{
-        myVideoRef.current.srcObject = ownVideo;
-        peer.on("call",call=>{
-            call.answer(ownVideo);
-            call.on("stream",friVideo =>{
-                friVideoRef.current.srcObject = friVideo;
-            })
+    });
+    const screenShare = async()=>{
+        await navigator.mediaDevices.getDisplayMedia({audio:false})
+        .then(ownStream=>{
+            shareCloser();
+            document.querySelector("#ownStream").srcObject = ownStream;
+            window.localStream = ownStream;
         })
-        socket.on("user-joined",({id})=>{
-            const call = peer.call(id,ownVideo);
-            call.on('stream',friVideo=>{
-                friVideoRef.current.srcObject = friVideo;
-            })
-            peers[id] = call;
+    }
+    const openCameraOnPhone = async()=>{
+        let cameraType = "user";
+        if(i%2)  cameraType = "enviroment";
+        await navigator.mediaDevices.getUserMedia({video:{facingMode: {exact:cameraType},},audio:true})
+        .then(ownStream=>{
+            shareCloser();
+            document.querySelector("#ownStream").srcObject = ownStream;
+            window.localStream = ownStream;
+            i++;
         });
-        socket.on("user-disconnect",({id})=>{
-            if(peers[id]) peers[id].close();
+    }
+    const openCamera = async()=>{
+        await navigator.mediaDevices.getUserMedia({video:{facingMode:"user",},audio:true})
+        .then(ownStream=>{
+            shareCloser();
+            document.querySelector("#ownStream").srcObject = ownStream;
+            window.localStream = ownStream;
+            i++;
+        });
+    }
+    const shareCloser = ()=>{
+        const stream = document.querySelector("#ownStream").srcObject;
+        if (stream) {
+          const tracks = stream.getTracks();
+          tracks.forEach(function (track) {
+            track.stop();
+          });
+          document.querySelector("#ownStream").srcObject = null;
+       }
+    }
+    peer.on("call",call=>{
+        call.answer(window.localStream);
+        call.on("stream",friendStream =>{
+            if(!videoUnique.includes(friendStream.id)){
+                const video = document.createElement("video");
+                video.setAttribute("id","friendStream");
+                document.querySelector("#friStreamCover").classList.replace("hidden","grid");
+                addStream(friendStream,video);
+                videoUnique.push(friendStream.id);
+            }
         })
     })
+    socket.on("user-joined",({id})=>{
+        callToNewFriend(id,window.localStream);
+        pushNotification();
+    });
+    socket.on("friend-screen",(id)=>{
+        friendChangeStream(window.localStream,id);
+    })
+    socket.on("user-disconnect",({id})=>{
+        if(peers[id]) peers[id].close();
+        document.querySelector("#friStreamCover").classList.replace("grid","hidden");
+        document.querySelector("#friendStream").remove();
+    });
+    const pushNotification = ()=>{
+        const show = new Notification("Hello Guy!",{
+            body:"Your friends was joined",
+            icon:"./Logo.jpg"
+        })
+        setTimeout(()=>{show.close();},5000);
+    };
+    const addStream = (stream,video)=>{
+        const friStreamCover = document.querySelector("#friStreamCover");
+        friStreamCover.classList.replace("hidden","grid");
+        video.classList.add("lg:w-[450px]","h-[360px]","rounded-lg");
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata",()=>{
+            video.play();
+        })
+        friStreamCover.append(video);
+    };
+    const friendChangeStream = (stream,id)=>{
+        const call = peer.call(id,stream);
+        call.on("stream",friendStream=>{
+            if(!videoUnique.includes(friendStream.id)){
+                const friendVideo = document.querySelector("#friendStream");
+                friendVideo.srcObject = friendStream;
+                friendVideo.addEventListener("loadedmetadata",()=>{
+                    friendVideo.play();
+                })
+                videoUnique.push(friendStream.id);
+            }
+            peers[id] = call;
+        })
+    }
+    const callToNewFriend = (id,stream)=>{
+        const call = peer.call(id,stream);
+        call.on("stream",friendStream =>{
+            if(!videoUnique.includes(friendStream.id)){
+                const friendVideo = document.createElement("video");
+                friendVideo.setAttribute("id","friendStream");
+                addStream(friendStream,friendVideo);
+                videoUnique.push(friendStream.id);
+            }
+        })
+        peers[id] = call;
+    }
+    const changeToScreen = async()=>{
+        await screenShare().then(()=>{
+            socket.emit("change-screen",window.id);
+        })
+    }
+    const changeToPhoneCamera =async ()=>{
+        await openCameraOnPhone().then(()=>{
+            socket.emit("change-screen",window.id);
+        })
+    }
+    const changeToCamera = async()=>{
+        await openCamera().then(()=>{
+            socket.emit("change-screen",window.id);
+        })
+    }
     return (
         <div className='w-full lg:px-0 px-[20px]'>
             <span className='flex justify-center items-center mt-10 md:text-4xl text-2xl font-extrabold'>Videos Calling</span>
-            <div className='mt-[10vh] grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1'>
-                <video className='rounded-lg md:w-[500px] md:h-[500px] sm:w-[300px] sm:h-[300px] w-[100%] h-[300px]' muted  ref={myVideoRef} autoPlay></video>
-                <video className='rounded-lg md:w-[500px] md:h-[500px] sm:w-[300px] sm:h-[300px]  w-[100%] h-[300px]' ref={friVideoRef} autoPlay></video>
+            <div id='video-grid' className='mt-[10vh] flex justify-center items-center md:gap-10 gap-5 md:flex-row flex-col'>
+                <div className='relative z-0 lg:w-[450px] lg:h-[350px] w-full h-auto bg-sky-300 rounded-lg grid place-content-center p-2 mx-10'>
+                    <video className='rounded-lg w-full h-full'
+                     muted  id="ownStream" autoPlay></video>
+                    <div className='absolute bottom-5 left-[40%] flex justify-center items-center gap-5'>
+                        <button>
+                            <AiFillVideoCamera onClick={changeToCamera} fill='gray' size="30px" className='sm:block hidden'/>
+                            <MdOutlineFlipCameraIos onClick={changeToPhoneCamera} fill='gray' size="30px" className='sm:hidden block'/>
+                        </button>
+                        <button>
+                            <SlScreenDesktop onClick={changeToScreen} fill='gray' size="30px"/>
+                        </button>
+                    </div>
+                </div>
+                <div id='friStreamCover' className='relative z-0 lg:w-[450px] lg:h-[350px] w-full h-auto bg-sky-300 rounded-lg hidden place-content-center p-2 mx-10'>
+                    
+                </div>
             </div>
         </div>
     );
